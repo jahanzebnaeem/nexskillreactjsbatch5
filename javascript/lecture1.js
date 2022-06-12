f2371c8f925a0d93dc3c8c3b185fd1fb8a1431cf
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-12 16:28:13 +0500

    Lecture 1
    Intro to javascript

---
+++ javascript/lecture1.js
@@ -0,0 +1,44 @@
+// alert("Hi my name is Jahanzeb");
+/* Hi how are you doing
+I am doing fine */
+
+// console.log("Hi how are you");
+
+// Input
+// var firstName = prompt("Please enter your First Name");
+// var lastName = prompt("Please enter your Last Name");
+
+// alert("My first name is " + firstName + ", and my sur name is " + lastName);
+
+// Data Types
+// console.log(typeof(10.0));
+// console.log(typeof('10'));
+// console.log(typeof(false));
+
+// Functions
+// function buyMilk(amountGiven, costPerBottle) {
+//     console.log("Move out of house");
+//     console.log("Turn left");
+//     console.log("Move forward");
+//     console.log("Move forward");
+//     console.log("Move forward");
+//     console.log("Move forward");
+//     console.log("Turn right");
+//     console.log("Enter shop");
+//     var noOfBottle = amountGiven/costPerBottle;
+//     console.log("Buy " + Math.floor(noOfBottle) + " bottles milk");
+//     console.log("Turn left");
+//     console.log("Move forward");
+//     console.log("Move forward");
+//     console.log("Move forward");
+//     console.log("Move forward");
+//     console.log("Turn right");
+//     return amountGiven%costPerBottle;
+// }
+
+// var changeReturn = buyMilk(500, 130);
+
+// alert(changeReturn);
+
+console.log(Math.floor(Math.random() * 100) + 1);
+
