c6547516575503cf8816425d0951a7cc9965950d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-26 05:11:51 +0500

    Lecture 5
    Introduction to ReactJs.

---
+++ UnderstandingReactJs/02-jsx-code-challenge/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favrouit Food</h1>
+    <ul>
+      <li>Chicken</li>
+      <li>Noodles</li>
+      <li>Mashrooms</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
