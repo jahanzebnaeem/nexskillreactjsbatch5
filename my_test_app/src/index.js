a415a4d641136b8e03e3d4baff1ba13084e0ba93
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-26 05:05:30 +0500

    Lecture 5
    Introduction to ReactJs

---
+++ my_test_app/src/index.js
@@ -0,0 +1,21 @@
+// jshint esversion:6
+
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// ReactDOM.render(<h1>Hello World</h1><p>This is a paragraph</p>, document.getElementById("root"));
+
+ReactDOM.render(
+    <div>
+      <h1>hello</h1>
+      <p>I am fine</p>
+    </div>,
+    document.getElementById("root")
+  )
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello";
+// document.getElementById("root").appendChild(h1);
