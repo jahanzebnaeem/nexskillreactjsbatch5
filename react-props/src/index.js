707f3e008feec02531fccc5246e33621a2502d2f
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-07-16 15:05:39 +0500

    Lecture 9
    Understating props.

--- react-props/src/index.js
+++ react-props/src/index.js
@@ -2,11 +2,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img
+        src={props.imageURL}
+        alt="avatar_img"
+      />
+      <p>{props.contactNumber}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
+    
+    <Card 
+      name="Beyonce"
+      imageURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      contactNumber="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <Card 
+      name="Jack Bauer" 
+      imageURL="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+    />
+    <Card 
+      name="Chuck Norris" 
+      imageURL="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+    />
 
-    <h2>Beyonce</h2>
+    {/* <h2>Beyonce</h2>
     <img
       src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
       alt="avatar_img"
@@ -28,7 +57,7 @@ ReactDOM.render(
       alt="avatar_img"
     />
     <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
+    <p>gmail@chucknorris.com</p> */}
   </div>,
   document.getElementById("root")
 );
