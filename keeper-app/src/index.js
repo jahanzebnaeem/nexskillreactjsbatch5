36f62850e9b98a52d11482618371840e6a7d0006
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-07-30 11:13:10 +0500

    Lecture 11
    Starting test files.

--- keeper-app/src/index.js
+++ keeper-app/src/index.js
@@ -9,15 +9,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-//1. Create a new React app.
-// npx create-react-app keeper-app
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-
-//HINT: You will need to study the classes in the styles.css file to apply styling.
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
