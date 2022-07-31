17041418a0f42f92be08153ccac804ff9d08a82b
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-07-31 14:36:45 +0500

    Lecture 12
    Keeper App code update + conditional rendering && ternary operators

--- conditional-rendering/src/components/App.jsx
+++ conditional-rendering/src/components/App.jsx
@@ -1,16 +1,79 @@
+// jshint esversion:6
+
 import React from "react";
+// import Login from "./Login";
+
+// var isLoggedIn = true;
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <h1>Hello</h1>
+//       <form className="form">
+//         <input type="text" placeholder="Username" />
+//         <input type="password" placeholder="Password" />
+//         <button type="submit">Login</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// function renderLoggedIn() {
+//   if (isLoggedIn === true) {
+//     return <h1>Hello</h1>
+//   } else {
+//     return (
+//       <form className="form">
+//         <input type="text" placeholder="Username" />
+//         <input type="password" placeholder="Password" />
+//         <button type="submit">Login</button>
+//       </form>
+//     );
+//   }
+// }
+
+// function App() {
+//   return <div className="container">
+//     {renderLoggedIn()}
+//   </div>
+// }
+
+// function App() {
+//   return <div className="container">
+//     if (isLoggedIn === true) {
+//       return <h1>Hello</h1>
+//     } else {
+//       return (
+//         <form className="form">
+//           <input type="text" placeholder="Username" />
+//           <input type="password" placeholder="Password" />
+//           <button type="submit">Login</button>
+//         </form>
+//       );
+//   }
+//   </div>
+// }
+
+// Expression && Expression
+
+// Expression && Expression
+// Condition && Expression
+
+// true && Expression
+// false
+
+const currentTime = new Date(2022, 7, 31, 17).getHours();
+console.log(currentTime);
+
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {/* {isLoggedIn ? <h1>Hello</h1> : <Login />} */}
+      {/* { currentTime > 16 ? <h1>Bye bye</h1> : <h2>Welcome</h2> } */}
+      { currentTime > 16 && <h1>Bye bye</h1> }
     </div>
-  );
+  )
 }
 
 export default App;
--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -4,12 +4,24 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
+
+// console.log(notes);
+
+// function createNotes(noteItem) {
+// 	return <Note title={noteItem.title} content={noteItem.content} />
+// }
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Note />
+			{/* <Note /> */}
+			{/* <Note title='This is a title' content='This is content'/> */}
+			{/* {notes.map(createNotes)} */}
+			{notes.map(noteItem => 
+				<Note title={noteItem.title} content={noteItem.content} />
+			)}
 			<Footer />
 		</div>
 	);
