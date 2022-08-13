7385311fc4df5d7773976023fb42b5a6b47238ae
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-08-13 08:54:02 +0500

    Lecture 13
    Initial commit to setup project.

---
+++ usestate-hook/src/components/App.jsx
@@ -0,0 +1,9 @@
+// jshint esversion:6
+
+import React from "react";
+
+function App() {
+  return <div />;
+}
+
+export default App;
---
+++ usestate-hook/src/index.js
@@ -0,0 +1,12 @@
+// jshint esversion:6
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div className="container">
+    <h1>0</h1>
+    <button>+</button>
+  </div>,
+  document.getElementById("root")
+);
