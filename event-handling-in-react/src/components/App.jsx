3f8e3ec61a78cbafb8c8910fb38bcdf13bba98a1
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-08-20 20:10:47 +0500

    Lecture 14
    Event handling and understanding synchronous and asynchronous processing.

--- event-handling-in-react/src/components/App.jsx
+++ event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+// jshint esversion:6
+import React, {useState} from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Submit");
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  function clickHandler() {
+    setHeadingText("Submitted");
+  }
+
+  function onMouseOver() {
+    setIsMouseOver(true);
+  }
+  
+  function onMouseOut() {
+    setIsMouseOver(false);
+  }
+
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{backgroundColor : isMouseOver ? "black" : "white"}} 
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+      onClick={clickHandler}>{headingText}</button>
     </div>
   );
 }
