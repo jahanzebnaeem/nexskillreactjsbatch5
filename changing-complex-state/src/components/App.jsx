ee6c613bbffebdf2d5cec4648cbe88791de47860
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-08-27 06:20:22 +0500

    Lecture 15
    Changing complex states and understanding react forms

--- changing-complex-state/src/components/App.jsx
+++ changing-complex-state/src/components/App.jsx
@@ -1,12 +1,74 @@
-import React from "react";
+// jshint esversion:6
+
+import React, {useState} from "react";
 
 function App() {
+  // const [fName, setFName] = useState("");
+  // const [lName, setLName] = useState("");
+
+  // function updateFName(event) {
+  //   // fName = event.target.value;
+  //   setFName(event.target.value);
+  // }
+
+  // function updateLName(event) {
+  //   // lName = event.target.value;
+  //   setLName(event.target.value);
+  // }
+
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    // const newValue = event.target.value;
+    // const inputName = event.target.name;
+    
+    // console.log(newValue);
+    // console.log(inputName);
+
+    // if (inputName === "fName") {
+    //   setFullName({fName: newValue});
+    // } else if (inputName === "lName") {
+    //   setFullName({lName: newValue});
+    // }
+
+    const { value, name } = event.target;
+
+    console.log(name, value);
+
+    setFullName(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      Hello {fullName.fName} {fullName.lName}
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input 
+          name="fName" 
+          placeholder="First Name" 
+          onChange={handleChange}
+          value={fullName.fName}
+        />
+        <input 
+          name="lName" 
+          placeholder="First Name" 
+          onChange={handleChange}
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+// jshint esversion:6
+
+import React, {useState} from "react";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [headerText, setHeaderText] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function submitHandler(event) {
+    setHeaderText(name);
+
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headerText}</h1>
+      <form onSubmit={submitHandler}>
+        <input 
+        onChange={handleChange} 
+        value={name}
+        type="text" 
+        placeholder="What's your name?" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
